988bf54ae578b94462b65b194bf8af03f4d85a7a
crazyjin7 <crazyjin7@90e1b522-1498-11df-b758-8308cf8b0e0b>
2010-02-10 11:07:43 +0000

    Ajax 를 이용한 검색(2) - js DOM 을 이용한 방식

--- NexianWeb/WebContent/js/searchById.js
+++ NexianWeb/WebContent/js/searchById.js
@@ -19,6 +19,31 @@ function searchById(e) {
 }
 
 function callback(responseXML) {
+	initialize();
+	
 	//화면에 조회결과를 출력
+	var results = responseXML.getElementsByTagName("member");
+	
+	var ulEl = document.createElement("ul");
+	for (var i = 0; i < results.length; i++) {
+		var nameEls = results[i].getElementsByTagName("name");
+		var name = nameEls[0].firstChild.nodeValue;
+		
+		var textEl = document.createTextNode(name);
+		var liEl = document.createElement("li");
+		liEl.appendChild(textEl);
+		ulEl.appendChild(liEl);
+	}
+	
+	document.getElementById("div_result").appendChild(ulEl);
+}
+
+function initialize() {
+	var divResultEl = document.getElementById("div_result")
+	var ulList = divResultEl.childNodes;
+	
+	for (var i = 0; i < ulList.length; i++) {
+		divResultEl.removeChild(ulList[i]);
+	}
 	
 }
